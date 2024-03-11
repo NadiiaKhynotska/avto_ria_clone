6635da32a39c1558ac33e27ebe9f60d2d7f2c0e2
NadiiaKhynotska <nadinyman@gmail.com>
2024-03-11 02:48:11 +0200

    add husky

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import configuration from './configs/config';
-import { HealthModule } from './modules/health/health.module';
-import { UserModule } from './modules/user/user.module';
+import configuration from '../configs/config';
+import { HealthModule } from './health/health.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
